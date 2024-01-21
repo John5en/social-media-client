3328fc3873703bcceaa1b3ffbd1ddaa2416baf3c
Eivind Johnsen <johnseneivind@hotmail.com>
2024-01-21 17:49:59 +0100

    added cypress tests

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,26 @@
+describe("Login and logout test", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.wait(500);
+    cy.get("#registerModal button[data-auth='login']").click();
+    cy.get("#loginModal", { timeout: 10000 }).should("be.visible");
+  });
+
+  it("logs in with wrong credentials", () => {
+    cy.get("#loginEmail").invoke("val", "dud@dud.noroff.no");
+    cy.get("#loginPassword").invoke("val", "dud");
+
+    cy.get("#loginModal button.btn-success").click();
+  });
+
+  it("logs in with valid credentials", () => {
+    cy.get("#loginEmail").invoke("val", "testuser1000@stud.noroff.no");
+    cy.get("#loginPassword").invoke("val", "testuser1000");
+
+    cy.get("#loginModal button.btn-success").click();
+  });
+
+  it("logs out using the logout button", () => {
+    cy.get('button[data-auth="logout"]').click({ force: true });
+  });
+});
